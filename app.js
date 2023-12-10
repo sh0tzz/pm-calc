71556c685874d3f66d336c76557384be88ca0b0b
sh0tzz <lubranier@gmail.com>
2023-12-10 13:37:00 +0100

    '.'

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class Calculator{
-    constructor() {
+    constructor(display, alt) {
+        this.display = display;
+        this.alt_display = alt;
         this.input = "";
         this.alt_text = "";
+        this.prev_ans = "";
         this.settings = {
             // angle-unit: "deg", "rad" 
             "angle-unit": "deg",
@@ -9,13 +12,49 @@ class Calculator{
             "num-format": "norm",
         }
     }
+
     evaluate() {
-        // Dorotea
-        console.log(this.input);
+        // nakon evaluacije i racunanja
+        // postaviti this.prev_ans
+        this.alt_text = this.input;
+        this.input = "";
+        this.update_display();
     }
+
+    update_display() {
+        this.display.value = this.input;
+        this.alt_display.value = this.alt_text;
+        this.display.focus();
+        this.display.setSelectionRange(this.display.value.length,this.display.value.length);
+    }
+
     insert(event) {
-        this.input += event.target.innerText;
+        this.input += event.currentTarget.innerText;
+        this.update_display();
+    }
+
+    insert_exponent(exponent) {
+        this.input += `^${exponent}`;
+        this.update_display();
+    }
+
+    backspace() {
+        this.input = this.input.slice(0, -1);
+        this.update_display();
+    }
+
+    clear() {
+        this.input = "";
+        this.alt_text = "";
+        this.update_display();
+    }
+
+    TODO(event) {
+        // window.alert(`Button ${event.currentTarget.innerText} not implemented`);
+        console.log(`Button ${event.currentTarget.innerText} not implemented`);
     }
 }
 
-const calc = new Calculator();
+const display = document.getElementById('input-field');
+const alt_display = document.getElementById('alt-text');
+const calc = new Calculator(display, alt_display);
--- math.js
+++ math.js
@@ -1,18 +1,6 @@
 // Vito
-// add
-// subtract
-// multiply
-// divide
-// power
-// root n-ti
-// log po bazi a
 // razlomak
-// sin, sin-1
-// cos, cos-1
-// tan, tan-1
-// ctg, ctg-1
-// abs (apsolutno)
-// factorial
+// ctg-1
 // binomial coefficient
 
 // Leon
@@ -25,3 +13,71 @@ function add(num1, num2) {
     return num1 + num2;
 }
 
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function power(num1, num2) {
+    return Math.pow(num1, num2);
+}
+
+function NthRoot(num1, num2) {
+    return Math.pow(num1, 1 / num2);
+}
+
+// function fraction(num1, num2 ) {
+//     return num1 / num2;
+// }
+
+function sin(num1){
+    return Math.sin(num1);
+}
+function asin(num1){
+    return Math.asin(num1);
+}
+
+function cos(num1){
+    return Math.cos(num1);
+}
+function acon(num1){
+    return Math.acos(num1);
+}
+
+function tan(num1){
+    return Math.tan(num1);
+}
+function atan(num1){
+    return Math.atan(num1);
+}
+
+function ctg(num1) {
+    return 1 / Math.tan(num1); 
+}
+
+function log(a, x) {
+    return Math.log(x) / Math.log(a);
+}
+
+function abs(num1){
+    return Math.abs(num1);
+}
+
+function factorial(n) {
+    if (n === 0 || n === 1) {
+        return 1;
+    } else {
+        let fact = 1;
+        for (let i = 2; i <= n; i++) {
+            fact *= i;
+        }
+        return fact;
+    }
+}
